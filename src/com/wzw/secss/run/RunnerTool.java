2a479b924c6f72d9fab2aea9665c889d012aee41
wzwllu <wzw1220@hotmail.com>
2022-12-14 19:57:13 +0800

    init

---
+++ src/com/wzw/secss/run/RunnerTool.java
@@ -0,0 +1,11 @@
+package com.wzw.secss.run;
+
+/**
+ * @ Author     ：wuzhengwei.
+ * @ Date       ：Created in 19:45 2022/12/14
+ * @ Description：
+ * @ Modified By：
+ * @Version: $
+ */
+public class RunnerTool {
+}
